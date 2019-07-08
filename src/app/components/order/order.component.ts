7d4cb77030101a32c116dc4a9fd7178e15a35d63
kveeresh49 <koppula.veeresh@gmail.com>
2019-07-08 08:35:00 +0530

    Update order.component.ts

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -14,10 +14,40 @@ export class OrderComponent implements OnInit {
   public quantity = 3;
   public x = new Date();
   public date = new Date().getDate() + '/' + new Date().getMonth() +'/'+ new Date().getFullYear();
+  imageUrls: any[];
   constructor(private bikeService: BikeserviceService,private toastr: ToastrService) { }
 
   ngOnInit() {
-    this.getAllOderDetails();
+  //  console.log(this.bikeService.orderImg);
+    this.productJson = this.bikeService.orderImg;
+
+
+
+    this.productJson.forEach((element,index) => {
+      this.imageUrls = [];
+      element.headerMasterlist.forEach((data,index1) => {
+        if(data.listDetails.length !== undefined && data.listDetails.length !==0) {
+
+                data.listDetails.forEach(element => {
+                  if(element.order == true){
+                    this.imageUrls.push({'imageUrl':element.imageUrl});
+                  }
+                });
+        }
+      })
+      this.productJson[index].img = this.imageUrls;
+    });
+    // this.bikeService.orderImg[0].headerMasterlist.forEach((data,index) => {
+    //     if(data.listDetails.length !== undefined && data.listDetails.length !==0) {
+    //       data.listDetails.forEach(element => {
+    //         if(element.order == true){
+    //           this.imageUrls.push({'imageUrl':element.imageUrl});
+    //         }
+    //       });
+
+    //     }
+    // })
+   console.log(this.productJson);
   }
 
   getAllOderDetails() {
